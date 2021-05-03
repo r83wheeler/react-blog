0e57043a98c6a371848b0c22fa1e6e37082b4cec
r83wheeler <63355207+r83wheeler@users.noreply.github.com>
2021-05-02 21:45:36 -0400

    added dropdown, info section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Dropdown from './components/Dropdown';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import { SliderData } from './data/SliderData';
 import 'bootstrap/dist/css/bootstrap.css';
 import GlobalStyle from './globalStyles';
+import InfoSection from './components/InfoSection';
+import { InfoData } from './data/InfoData';
 
 
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
   return (
    <>
       <GlobalStyle />
-      <Navbar />
+      <Navbar toggle={toggle}/>
+      <Dropdown isOpen={isOpen} toggle={toggle} />
       <Hero slides={SliderData}/>
+      <InfoSection {...InfoData} />
    </>
   );
 }
